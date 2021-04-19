57031488ea956c6f64e563e1d7b547cbe2417af2
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-04-19 03:47:05 +0200

    small change

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -36,16 +36,6 @@ export const Navigation = (props) => {
               </a>
             </li>
             <li>
-              <a href='#services' className='page-scroll'>
-                Services
-              </a>
-            </li>
-            <li>
-              <a href='#portfolio' className='page-scroll'>
-                Gallery
-              </a>
-            </li>
-            <li>
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
