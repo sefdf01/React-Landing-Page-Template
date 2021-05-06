6e3495d9c5b130a03a4d02a5e20309eb5f063ece
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-05-06 16:26:55 +0200

    Revert "test commit?"
    This reverts commit 7592ea4dac509bef72564fac4674fae7cf914405.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Landing from './Landing';
-import Register from './Register'
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
