7592ea4dac509bef72564fac4674fae7cf914405
sefdf01 <34137064+sefdf01@users.noreply.github.com>
2021-05-06 16:23:57 +0200

    test commit?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Landing from './Landing';
+import Register from './Register'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Register/>
   </React.StrictMode>,
   document.getElementById('root')
 );
